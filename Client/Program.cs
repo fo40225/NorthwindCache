cb70c566811e3466d19a821e117500ddef2e61e8
fo40225 <fo40225@hotmail.com>
2015-06-21 15:30:08 +0800

    Main Code

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Process pipeServer = new Process();
+            pipeServer.StartInfo.FileName = "Server.exe";
+
+            AnonymousPipeServerStream pipeQuery = new AnonymousPipeServerStream(PipeDirection.Out,
+                    HandleInheritability.Inheritable);
+
+            AnonymousPipeServerStream pipeAnswer = new AnonymousPipeServerStream(PipeDirection.In,
+                    HandleInheritability.Inheritable);
+
+            pipeServer.StartInfo.Arguments = pipeQuery.GetClientHandleAsString() + " " + pipeAnswer.GetClientHandleAsString();
+            pipeServer.StartInfo.UseShellExecute = false;
+            pipeServer.Start();
+
+            pipeQuery.DisposeLocalCopyOfClientHandle();
+            pipeAnswer.DisposeLocalCopyOfClientHandle();
+
+            StreamWriter sr = new StreamWriter(pipeQuery);
+            sr.AutoFlush = true;
+
+            BinaryReader sw = new BinaryReader(pipeAnswer);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            for (; ; )
+            {
+                Console.WriteLine("Input CategoryID:");
+                var CategoryID = Console.ReadLine();
+                sr.WriteLine(CategoryID);
+
+                var results = bf.Deserialize(pipeAnswer) as Dictionary<string, object>;
+
+                object CategoryName;
+                results.TryGetValue("CategoryName", out CategoryName);
+
+                object Description;
+                results.TryGetValue("Description", out Description);
+
+                Console.WriteLine(CategoryName);
+                Console.WriteLine(Description);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PipeStream pipeQuery =
+                    new AnonymousPipeClientStream(PipeDirection.In, args[0]);
+
+            PipeStream pipeAnswer =
+                    new AnonymousPipeClientStream(PipeDirection.Out, args[1]);
+
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+            sb.DataSource = ".";
+            sb.IntegratedSecurity = true;
+            sb.InitialCatalog = "Northwind";
+
+            string strConn = sb.ToString();
+
+            string strCmd = @"SELECT [CategoryID]
+                                   , [CategoryName]
+                                   , [Description]
+                                   , [Picture]
+                                FROM [Northwind].[dbo].[Categories];";
+
+            DataTable localDataCache = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter(strCmd, conn))
+                {
+                    da.Fill(localDataCache);
+                }
+            }
+
+            DataView cacheIndexed = new DataView(localDataCache);
+            cacheIndexed.Sort = "CategoryID";
+
+            StreamReader sr = new StreamReader(pipeQuery);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            for (; ; )
+            {
+                var CategoryID = Convert.ToInt32(sr.ReadLine());
+                var results = cacheIndexed.FindRows(CategoryID);
+                var Category = results.SingleOrDefault();
+                if (Category == null)
+                {
+                    bf.Serialize(pipeAnswer, new Dictionary<string, object>());
+                    pipeAnswer.Flush();
+                }
+                else
+                {
+                    Dictionary<string, object> rtn = new Dictionary<string, object>();
+                    rtn.Add("CategoryID", Category["CategoryID"]);
+                    rtn.Add("CategoryName", Category["CategoryName"]);
+                    rtn.Add("Description", Category["Description"]);
+                    rtn.Add("Picture", Category["Picture"]);
+                    bf.Serialize(pipeAnswer, rtn);
+                    pipeAnswer.Flush();
+                }
+            }
+        }
+    }
+}
